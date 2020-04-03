1f18b87daea6b3bdcef3c834a5e2e52caa6126e1
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-03 19:04:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,20 +51,16 @@ class Dog
   end 
   
   def self.find_or_create_by(name:, breed:)
-    sql = <<-SQL
-      SELECT * FROM dogs
-      WHERE name = ? AND breed = ?
-      SQL
+    
+    sql = "SELECT * FROM dogs WHERE name=?, breed=?"
+    dog = DB[:conn].execute(sql, name, breed).first
 
-
-      dog = DB[:conn].execute(sql, name, breed).first
-
-      if dog
-        new_dog = self.new_from_db(dog)
-      else
-        new_dog = self.create({:name => name, :breed => breed})
-      end
-      new_dog
+    if dog
+      new_dog = self.new_from_db(dog)
+    else
+      new_dog = self.create({:name => name, :breed => breed})
+    end
+    new_dog
   end
   
   
