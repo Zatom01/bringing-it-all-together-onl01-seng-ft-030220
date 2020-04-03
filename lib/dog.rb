788e23a147cda3e1d4f198a46b154d70cc5a4f57
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-03 19:05:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,12 @@ class Dog
   end 
   
   def self.find_or_create_by(name:, breed:)
-    
-    sql = "SELECT * FROM dogs WHERE name=?, breed=?"
+    sql = "<<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL"
+
+
       dog = DB[:conn].execute(sql, name, breed).first
 
       if dog
