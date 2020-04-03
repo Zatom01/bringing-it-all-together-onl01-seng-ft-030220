f1296b3657c59e60a2ffb07f867327908ea9c69c
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-03 19:06:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,14 +57,14 @@ class Dog
       SQL
 
 
-      dog = DB[:conn].execute(sql, name, breed).first
+    dog = DB[:conn].execute(sql, name, breed).first
 
-      if dog
-        new_dog = self.new_from_db(dog)
-      else
-        new_dog = self.create({:name => name, :breed => breed})
-      end
-      new_dog
+    if dog
+      new_dog = self.new_from_db(dog)
+    else
+      new_dog = self.create({:name => name, :breed => breed})
+    end
+    new_dog
   end
   
   
